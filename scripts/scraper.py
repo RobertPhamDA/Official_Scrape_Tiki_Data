b6ba0b4453bf4e7f1de8417c012aa98110891375
RobertPhamDA <phamdacthanh2503@gmail.com>
2025-06-14 15:54:20 +0700

    Finnal

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -30,7 +30,7 @@ def extract_products(sbd_start: int = 34000001, sbd_end: int = 34000210) -> pd.D
 
 def load_to_postgres(df: pd.DataFrame):
     db_user = os.getenv("POSTGRES_USER", "airflow")
-    db_pass = os.getenv("POSTGRES_PASSWORD", "airflow_pass")
+    db_pass = os.getenv("POSTGRES_PASSWORD", "airflow")
     db_host = os.getenv("POSTGRES_HOST", "postgres")
     db_port = os.getenv("POSTGRES_PORT", "5432")
     db_name = os.getenv("POSTGRES_DB", "airflow")
